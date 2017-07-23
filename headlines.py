c7284eb08773e294ed81afd7401f3b09659591d7
Sourabh <sourabhpandey05@gmail.com>
2017-07-23 11:31:56 +0530

    advance templating

--- headlines.py
+++ headlines.py
@@ -17,8 +17,8 @@ RSS_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
 	feed = feedparser.parse(RSS_FEED[publication])  
 	first_article = feed['entries'][0]  #return """<html>    <body>        <h1> BBC Headlines </h1>        <b>{0}</b> <br/>        <i>{1}</i> <br/>        <p>{2}</p> <br/>    </body></html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
-	return render_template("home.html",title=first_article.get("title"), published=first_article.get("published"), summary=first_article.get("summary"))
-
+	#return render_template("home.html",title=first_article.get("title"), published=first_article.get("published"), summary=first_article.get("summary"))
+	return render_template("home.html",articles=feed['entries'])
 
 if __name__== '__main__':
 	app.run(port=5000,debug='True')
